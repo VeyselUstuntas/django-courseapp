a1c3f5099a5505d2d07d5f17ebe41a4911d0ceba
veyselustuntas <veyselustuntas@hotmail.com>
2024-08-07 14:36:13 +0300

    static html tag

--- courses/views.py
+++ courses/views.py
@@ -10,7 +10,16 @@ data = {
 
 
 def coursesList(request):
-    return HttpResponse("Courses List")
+    category_list = list(data.keys())
+    list_items = ""
+    for category in category_list:
+        redirect_url = reverse('courses_by_category',args=[category])
+        list_items+=f"<li> <a href='{redirect_url}'> {category} </a> </li>"
+    
+
+    html = f"<h1>Courses List</h1> <br> <ul>{list_items}</ul>"
+    return HttpResponse(html)
+
 
 def courseDetail(request,course_name):
     return HttpResponse(f"{course_name} Course Details")
