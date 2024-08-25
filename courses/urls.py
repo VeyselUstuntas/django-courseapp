b9c7590d70da2492e370449d5e05c8c69b388d59
veyselustuntas <veyselustuntas@hotmail.com>
2024-08-26 01:32:42 +0300

    ignore

--- courses/urls.py
+++ courses/urls.py
@@ -3,9 +3,6 @@ from courses import views
 
 urlpatterns = [
     path("",views.index),
-    path("<slug:slug>",views.courseDetail,name="course_detail"),
-    path("category/<int:category_id>",views.getCoursesByCategoryId),
-    path("category/<str:category_name>",views.getCoursesByCategoryName, name="courses_by_category"),
 
 ]
         
