8b1ff22bc54f0ffa26a2c26e88855538f87b8292
veyselustuntas <veyselustuntas@hotmail.com>
2024-10-10 18:59:54 +0300

    user registration completed form classes created

--- account/forms.py
+++ account/forms.py
@@ -12,3 +12,30 @@ class AccountLoginForm(forms.Form):
         widget= forms.PasswordInput(attrs={"class":"form-control"}),
         error_messages={"required":"Şifre Girilmelidir"},
     )
+
+
+class AccountRegisterForm(forms.Form):
+    username = forms.CharField(
+        label = "Username",
+        widget = forms.TextInput(attrs={"class":"form-control"}),
+        error_messages = {"required":"Kullanıcı Adı Girilmelidir"},
+    )
+
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={"class": "form-control"}),
+        error_messages={"required": "Email Girilmelidir", "invalid": "Geçerli bir email adresi giriniz."},
+    )
+
+
+    password = forms.CharField(
+        label = "Password",
+        widget= forms.PasswordInput(attrs={"class":"form-control"}),
+        error_messages={"required":"Şifre Girilmelidir"},
+    )
+
+    repassword = forms.CharField(
+        label = "Re-Password",
+        widget= forms.PasswordInput(attrs={"class":"form-control"}),
+        error_messages={"required":"Şifre Girilmelidir"},
+    )
--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
-from account.forms import AccountLoginForm
+from django.contrib.auth.models import User
+from account.forms import AccountLoginForm, AccountRegisterForm
+from django.db.models import Q
 
 
 def user_login(request):
+    form = AccountLoginForm()
+
     if request.method == "POST":
         form = AccountLoginForm(request.POST)
 
@@ -15,16 +19,33 @@ def user_login(request):
             if user is not None:
                 login(request, user)
                 return redirect("courses")
-            else:
-                form = AccountLoginForm(initial={"username":username})
-                return render(request,"account/login.html",{"form":form,"error":"Username ya da Parola Yanlış"})
-    else:
-        form = AccountLoginForm()
+            return render(request,"account/login.html",{"form":form,"error":"Username or Password Incorrect"})
+        
     return render(request,"account/login.html",{"form":form})
 
 
 def user_register(request):
-    return render(request,"account/register.html")
+    if request.method == "POST":
+        form = AccountRegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            repassword = form.cleaned_data.get("repassword")
+            if User.objects.filter(Q(username=username) | Q(email=email)).exists(): # kullanıcı varlığı sorgulama
+                form = AccountRegisterForm(initial={"username":username,"email":email})
+                return render(request,"account/register.html",{"form":form,"error":"User Account Available with Entered Username/Email"})
+
+            if password == repassword:
+                user = User.objects.create_user(username=username,email=email,password=password,is_staff=False, is_active=True, is_superuser=False)
+                user.save()
+            else:
+                form = AccountRegisterForm(initial={"username":username,"email":email})
+                return render(request,"account/register.html",{"form":form,"error":"Passwords Do Not Match"})
+            return redirect("user_login")
+    else:
+        form = AccountRegisterForm()
+    return render(request,"account/register.html",{"form":form})
 
 
 def user_logout(request):
