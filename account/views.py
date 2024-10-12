cbc844585f7908f49c091e87a7e1cccea740fdaa
veyselustuntas <veyselustuntas@hotmail.com>
2024-10-13 01:26:43 +0300

    messages

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from account.forms import AccountLoginForm, AccountRegisterForm
 from django.db.models import Q
+from django.contrib import messages
 
 
 def user_login(request):
@@ -23,9 +24,11 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
+                messages.add_message(request, messages.SUCCESS, f"Welcome, {request.user.username}")
                 if next_url:
                     return redirect(next_url)
                 return redirect("courses")
+            messages.add_message(request,messages.ERROR ,"Username or Password Incorrect")
             return render(request,"account/login.html",{"form":form,"error":"Username or Password Incorrect"})
         
     return render(request,"account/login.html",{"form":form})
@@ -41,11 +44,13 @@ def user_register(request):
             repassword = form.cleaned_data.get("repassword")
             if User.objects.filter(Q(username=username) | Q(email=email)).exists(): # kullanıcı varlığı sorgulama
                 form = AccountRegisterForm(initial={"username":username,"email":email})
+                messages.add_message(request,messages.ERROR,"User Account Available with Entered Username/Email")
                 return render(request,"account/register.html",{"form":form,"error":"User Account Available with Entered Username/Email"})
 
             if password == repassword:
                 user = User.objects.create_user(username=username,email=email,password=password,is_staff=True, is_active=True, is_superuser=True)
                 user.save()
+                messages.add_message(request,messages.WARNING,"Hesap Oluşturuldu Giriş Yapabilirsiniz.")
             else:
                 form = AccountRegisterForm(initial={"username":username,"email":email})
                 return render(request,"account/register.html",{"form":form,"error":"Passwords Do Not Match"})
@@ -57,4 +62,5 @@ def user_register(request):
 
 def user_logout(request):
     logout(request)
+    messages.add_message(request,messages.INFO,"Çıkış Yapıldı.")
     return redirect("courses")
